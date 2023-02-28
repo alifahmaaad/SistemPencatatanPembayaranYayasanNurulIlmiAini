35699b2ffcebc8b3a1b42c20a83f2928275740dc
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-02-28 11:23:50 +0700

    add datagrid for table

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import { useState } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-// import Database from "tauri-plugin-sql-api";
 import Home from "./pages/Home";
 import "./index.css";
 import SidebarComponent from "./component/SidebarComponent";
 function App() {
-  // async function greet() {
-  //   const db = await Database.load("sqlite:haiya.db");
-  //   await db.execute(`CREATE TABLE IF NOT EXISTS users (
-  //     id INTEGER PRIMARY KEY AUTOINCREMENT,
-  //     name TEXT NOT NULL,
-  //     email TEXT NOT NULL
-  //   )`);
-  // }
-
   return (
     <BrowserRouter>
-      <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
+      <div id="app">
         <SidebarComponent />
         <Routes>
           <Route path="/" element={<Home />} />
