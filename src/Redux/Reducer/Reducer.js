fc567a3ddd7828ee4ae430eb3b156d6d9dc5ada0
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-03-09 15:55:14 +0700

    add more reducer in react redux

--- src/Redux/Reducer/Reducer.js
+++ src/Redux/Reducer/Reducer.js
@@ -1,19 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
+// console.log(localStorage.getItem("login"));
+
+const getlogin = () => {
+  if (localStorage.getItem("login") == "false") {
+    return false;
+  } else if (localStorage.getItem("login") == null) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 export const loginSlice = createSlice({
   name: "loginhandle",
   initialState: {
-    status: false,
+    isLogin: getlogin(),
+    isSetup: localStorage.getItem("setup"),
+    role: 1,
   },
   reducers: {
-    login: (state, check) => {
-      console.log(check.payload);
-      state.status = !state.status;
+    login: (state) => {
+      state.isLogin = !state.isLogin;
+      localStorage.setItem("login", state.isLogin);
+    },
+    setupdb: (state, value) => {
+      state.isSetup = value.payload;
+      localStorage.setItem("setup", value.payload);
+    },
+    role: (state, value) => {
+      state.role = value.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { login } = loginSlice.actions;
+export const { login, setupdb, role } = loginSlice.actions;
 
 export default loginSlice.reducer;
